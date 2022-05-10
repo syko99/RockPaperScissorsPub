0f5ae9b397bb3ab3e64ab994405d6832a6642bcc
syko99 <sykong.yang@hotmail.com>
2022-05-09 20:24:56 -0500

    add comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let hands = ['rock', 'paper', 'scissors'];
 
+//randomly selects hand for computer and returns it
 function computerPlay() {
     let index = Math.floor(Math.random() * hands.length);
     let computerSelection = hands[index];
     return computerSelection;
 }
 
+//prompts for user input and uses for player's hand in play function
 function initiate() {
    let playerSelection = prompt("Choose Rock, Paper, or Scissors: ");
    playerSelection = playerSelection.toLowerCase();
@@ -15,6 +17,7 @@ function initiate() {
    return play(playerSelection,computerPlay());
 }
 
+//compares user hand to computer hand and returns winner and score value.
 function play(playerSelection, computerSelection) {
 
     let win = `You won this round! ${playerSelection} beats ${computerSelection}`;
@@ -65,6 +68,7 @@ function play(playerSelection, computerSelection) {
     }
 }
 
+//plays game for 5 rounds, keeping score, best of 5.
 function game() {
     
     let playerScore = 0;
