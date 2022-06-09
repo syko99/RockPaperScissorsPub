44a0cc258da89bc5e7ed61d197f50799da9dc273
syko99 <sykong.yang@hotmail.com>
2022-06-09 14:16:49 -0500

    almost finished

--- script.js
+++ script.js
@@ -1,9 +1,71 @@
 let hands = ['rock', 'paper', 'scissors'];
 
+//user hand buttons
+let rockBtn = document.querySelector("#rock");
+let paperBtn = document.querySelector("#paper");
+let scissorsBtn = document.querySelector("#scissors");
+let buttons = document.querySelectorAll(".hands");
+let reset = document.querySelector("#reset");
+
+//score keeping
+let round = 1;
+let userScore = 0;
+let compScore = 0;
+let userScoreText = document.querySelector("#score__user");
+let compScoreText = document.querySelector("#score__comp");
+
+// UI components
+let message = document.querySelector("#msg");
+let computerEmoji = document.querySelector("#hand__comp-emoji");
+let roundText = document.querySelector("#round");
+
+
+rockBtn.addEventListener("click", function(e){
+    play(this.id, computerPlay());
+    round++;
+    if(round >= 5){
+        displayWinner();
+        disableButtons();
+    }
+});
+paperBtn.addEventListener("click", function(e){
+    play(this.id, computerPlay());
+    round++;
+    if(round >= 5){
+        displayWinner();
+        disableButtons();
+    }
+});
+scissorsBtn.addEventListener("click", function(e){
+    play(this.id, computerPlay());
+    round++;
+    if(round >= 5){
+        displayWinner();
+        disableButtons();
+    }
+});
+reset.addEventListener("click", ()=>{
+    round = 0;
+    userScore = 0;
+    compScore = 0;
+    userScoreText.textContent = `You: ${userScore}`
+    compScoreText.textContent = `Computer: ${compScore}`
+    computerEmoji.textContent = "🖥️"
+    message.textContent = "";
+    enableButtons();
+})
+
 //randomly selects hand for computer and returns it
 function computerPlay() {
     let index = Math.floor(Math.random() * hands.length);
     let computerSelection = hands[index];
+    if (computerSelection == 'rock'){
+        computerEmoji.textContent = "✊"
+    } else if(computerSelection == 'paper'){
+        computerEmoji.textContent = "🖐️";
+    } else {
+        computerEmoji.textContent = "✌️";
+    }
     return computerSelection;
 }
 
@@ -19,80 +81,83 @@ function initiate() {
 
 //compares user hand to computer hand and returns winner and score value.
 function play(playerSelection, computerSelection) {
-
-    let win = `You won this round! ${playerSelection} beats ${computerSelection}`;
-    let lose = `You lost this round! ${computerSelection} beats ${playerSelection}`;
-    let draw = `There was a draw!`;
-
+    userScoreText.style.animation = "none";
+    compScoreText.style.animation = "none";
     if (playerSelection == 'rock') {
         if (computerSelection == 'scissors') {
-            console.log(win);
-            return 1;
+            userPoint();
         }
         if (computerSelection == 'paper') {
-            console.log(lose);
-            return -1;
+            compPoint();
         }
         else{
-            console.log(draw);
-            return 0;
+            roundText.textContent = `Round: ${round}`;
+            return;
         }
     } 
     if (playerSelection == 'paper') {
         if (computerSelection == 'rock') {
-            console.log(win);
-            return 1;
+            userPoint();
         }
         if (computerSelection == 'scissors') {
-            console.log(lose);
-            return -1;
+            compPoint();
         }
         else{
-            console.log(draw);
-            return 0;
+            roundText.textContent = `Round: ${round}`;
+            return;
         }
     }
     if (playerSelection == 'scissors') {
         if (computerSelection == 'paper') {
-            console.log(win);
-            return 1;
+            userPoint();
         }
         if (computerSelection == 'rock') {
-            console.log(lose);
-            return -1;
+            compPoint();
         }
         else{
-            console.log(draw);
-            return 0;
+            roundText.textContent = `Round: ${round}`;
+            return;
         }
     }
 }
-
-//plays game for 5 rounds, keeping score, best of 5.
-function game() {
-    
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++){
-        let point = initiate();
-        if (point == 1){
-            playerScore += 1;
-        }
-        if (point == -1){
-            computerScore += 1;
-        }
-        console.log(`You: ${playerScore} \t Comp: ${computerScore}`);
+function userPoint(){
+    userScore += 1;
+    userScoreText.style.animation = "greenText 250ms forwards";
+    userScoreText.textContent = `You: ${userScore}`;
+    roundText.textContent = `Round: ${round}`;
+}
+function compPoint(){
+    compScore += 1;
+    compScoreText.style.animation = "redText 250ms forwards";
+    compScoreText.textContent = `Computer: ${compScore}`;
+    roundText.textContent = `Round: ${round}`;
+}
+function displayWinner(){
+    if (userScore > compScore){
+        message.textContent = "You Won!"
+    } else if (userScore < compScore){
+        message.textContent = "You Lost!"
     }
-    if(playerScore > computerScore){
-        return 'You won the game!';
-    }
-    if(computerScore > playerScore){
-        return 'You lost the game!';
-    }
-    else {
-        return "It's a Draw!";
+    else{
+        message.textContent = "It's a Draw!"
     }
 }
 
-console.log(game());
+function disableButtons(){
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    buttons.forEach((hand)=>{
+        hand.classList.remove("handhover");
+    });
+}
+function enableButtons(){
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+    buttons.forEach((hand)=>{
+        hand.classList.add("handhover");
+    });
+    round = 1;
+    roundText.textContent = `Round: ${round}`;
+}
